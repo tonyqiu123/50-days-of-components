12504854a7698df5a215f6a9cd36c513742d01bd
tonyqiu123 <tonyqiu12345@gmail.com>
2023-08-17 01:23:33 -0400

    Added error handling for db connection

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,34 @@
 const express = require('express');
-const { pool } = require('./db.js')
+const { pool } = require('./db.js');
 
 const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+  } else {
+    console.log('Connected to database');
+    connection.release(); 
+  }
+});
 
 app.get('/', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM users')
-  res.json(rows)
-})
+  const [rows] = await pool.query('SELECT * FROM users');
+  res.json(rows);
+});
 
 app.get('/ping', async (req, res) => {
-  const [result] = await pool.query('SELECT "hello world" as RESULT')
-  res.json(result[0])
-})
+  const [result] = await pool.query('SELECT "hello world" as RESULT');
+  res.json(result[0]);
+});
 
 app.post('/create', async (req, res) => {
-  const result = await pool.query('INSERT INTO users(name) VALUES("John")')
-  res.json(result)
-  res.send('Welcome to Server')
-})
+  const result = await pool.query('INSERT INTO users(name) VALUES("John")');
+  res.json(result);
+  res.send('Welcome to Server');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
