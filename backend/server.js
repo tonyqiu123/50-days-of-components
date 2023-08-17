8958ba6b0e5638ba528b8b0d72cf358916568642
tonyqiu123 <tonyqiu12345@gmail.com>
2023-08-17 00:59:18 -0400

    Updated server.js port to process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = 3000;  
+const port = process.env.PORT || 3000;
 
 // MySQL Database Configuration
 const db = mysql.createConnection({
