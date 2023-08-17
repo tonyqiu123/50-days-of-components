8dcd29304dce478850db0fe1c624ba35688655be
tonyqiu123 <tonyqiu12345@gmail.com>
2023-08-17 00:33:40 -0400

    Updated server.js database name

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const port = 3000; // Change this to your preferred port number
+const port = 3000;  
 
 // MySQL Database Configuration
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'tonyqiu12345',
-  database: 'your_database_name'
+  database: '100_days_of_components'
 });
 
 // Connect to MySQL Database
