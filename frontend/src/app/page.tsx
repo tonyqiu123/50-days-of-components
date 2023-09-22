48718231823c1c7c168f24985985419ff23a041a
tonyqiu123 <tonyqiu12345@gmail.com>
2023-09-22 13:43:41 -0400

    added script tag to traffic tracking file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,52 +1,13 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import Button from '@/components/Button/Button';
-import { NavBar, NavBarLeft, NavBarRight } from '@/components/NavBar/NavBar';
-import Image from 'next/image';
-import Icon from '@/components/Icon/Icon';
-import SearchBar from '@/components/SearchBar/SearchBar';
-import Input from '@/components/Input/Input';
+import Button from '@/components/Button/Button'; 
 import TextArea from '@/components/TextArea/TextArea';
 
 
 export default function Home() {
 
-
   const [isDarkMode, setIsDarkMode] = useState(true)
-  const [componentIdea, setComponentIdea] = useState('')
-  const [search, setSearch] = useState<string>('')
-
-  const componentNames = [
-    "Accordion",
-    "Backdrop",
-    "Badge",
-    "Button",
-    "Carousel",
-    "Checkbox",
-    "Command",
-    "DragNDrop",
-    "Dropdown",
-    "HoverCard",
-    "Icon",
-    "Input",
-    "Modal",
-    "NavBar",
-    "OutsideClick",
-    "Popover",
-    "PrettyCode",
-    "SearchBar",
-    "Sheet",
-    "ShowMore",
-    "Skeleton",
-    "Slider",
-    "Switch",
-    "Tabs",
-    "TextArea",
-    "Toast",
-    "Tooltip"
-  ];
-
 
   return (
 
